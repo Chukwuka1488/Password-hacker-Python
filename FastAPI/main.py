6b9a940e98f12f6212ac7a65aa1a117bc622568f
Chuks Akibot <juniorakibor@yahoo.com>
2021-11-19 10:21:33 +0100

    get method and post method in fast api

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,7 +1,10 @@
 # source venv/bin/activate
 
 # pip install -r requirements.txt
+
+# uvicorn main:app --reload
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -9,3 +12,14 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"message": "get a post"}
+
+
+@app.post("/createPosts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
